5f6167158357dc728279769f1a8877d2fa568990
Vinicius Solique <viniciussolique39@gmail.com>
2024-10-18 09:15:46 -0400

    Usando a extensão thunder client

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ import express from 'express'
 
 const app = express()
 
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    console.log(req)
+
+    res.send('Ok, aqui deu bom')
+
+})
+
 app.get('/usuarios', (req, res) =>{
     res.send('Ok, deu certo')
 }) 
