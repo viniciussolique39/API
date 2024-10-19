51c8d7dddfb11a43c43c706498a08d127f59b0bd
Vinicius Solique <viniciussolique39@gmail.com>
2024-10-18 20:09:03 -0400

    Criação do banco de dados Mongodb e utilizando o prisma.io

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
@@ -14,7 +17,7 @@ app.post('/usuarios', (req, res) => {
 })
 
 app.get('/usuarios', (req, res) =>{
-    res.json(users)
+    res.status(200).json(users)
 }) 
 
 app.listen(3000)
