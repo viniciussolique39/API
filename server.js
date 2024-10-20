70e71998d55a220a42e905f4540e8242aec7c1d7
Vinicius Solique <viniciussolique39@gmail.com>
2024-10-19 20:18:59 -0400

    Criação da tabela User

--- server.js
+++ server.js
@@ -10,18 +10,20 @@ const users = []
 
 app.post('/usuarios', async (req, res) => {
 
-    await prisma.user.create({
+     prisma.user.create({
+
         data: {
             email: req.body.email,
             name:  req.body.name,
-            age:   req.body.age
+            age: req.body.age
+        
         }
     })
 
-    users.push(req.body)
-
+res.status(201).json(req.body)
 
 })
+
 
 app.get('/usuarios', (req, res) =>{
     res.status(200).json(users)
